3210f2d049d5fe5761476b3654f4465429564333
Daniel Zerihun Geda <152178755+DanielZerihunGeda@users.noreply.github.com>
2025-12-16 16:30:49 +0300

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -19,6 +19,7 @@ class Settings(BaseSettings):
   tg_api_id: int
   tg_api_hash: str
   tg_bot_token: str
+  grok_api_key: str
 
   model_config = SettingsConfigDict(env_file=".env", env_file_encoding='utf-8',
    extra = 'allow')
